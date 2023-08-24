001a1a06366506453ad75cc456f19b913ddb57b6
Dasom Park <dev.somda@gmail.com>
2023-08-24 21:56:35 +0900

    FEAT: 로그인 기능 구현 및 회원가입과 공통 컴포넌트 분리
    Co-authored-by: devseop <cocc263@gmail.com>

--- src/api/requests.tsx
+++ src/api/requests.tsx
@@ -1,9 +1,17 @@
 import React from 'react';
 import { instance } from './axiosInstance';
-import { IAuthInfo } from '../types/api';
+import { IAuthInfo, ISignInResponse } from '../types/api';
 
 export const postSignUp = async ({ email, password }: IAuthInfo) => {
   const res = await instance.post('/auth/signup', {
+    email,
+    password,
+  });
+  return res;
+};
+
+export const postSignIn = async ({ email, password }: IAuthInfo) => {
+  const res = await instance.post('/auth/signin', {
     email,
     password,
   });
--- src/routes/Router.tsx
+++ src/routes/Router.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { Routes, Route, BrowserRouter } from 'react-router-dom';
 import Main from '../pages/Main';
 import SignUp from '../pages/SignUp';
+import SignIn from '../pages/SignIn';
 
 export default function Router() {
   return (
@@ -9,6 +10,7 @@ export default function Router() {
       <Routes>
         <Route path='/' element={<Main />} />
         <Route path='/signup' element={<SignUp />} />
+        <Route path='/signin' element={<SignIn />} />
       </Routes>
     </BrowserRouter>
   );
