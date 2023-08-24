ab06a3b9a2986abd4d9576b7e7aba0f4481442b9
Dasom Park <dev.somda@gmail.com>
2023-08-24 21:01:52 +0900

    FEAT: 회원가입 관련 변수명 변경
    Co-authored-by: devseop <cocc263@gmail.com>

--- src/api/requests.tsx
+++ src/api/requests.tsx
@@ -3,9 +3,9 @@ import { instance } from './axiosInstance';
 import { IAuthInfo } from '../types/api';
 
 export const postSignUp = async ({ email, password }: IAuthInfo) => {
-  const response = await instance.post('/auth/signup', {
+  const res = await instance.post('/auth/signup', {
     email,
     password,
   });
-  return response.data;
+  return res;
 };
