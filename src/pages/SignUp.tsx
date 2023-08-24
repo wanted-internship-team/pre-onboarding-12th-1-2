5e3930c5c0e03804db1c437d4d326c36752de2f0
Dasom Park <dev.somda@gmail.com>
2023-08-24 21:02:35 +0900

    FEAT: 유효성 검증에 따른 버튼 활성화 구현
    Co-authored-by: devseop <cocc263@gmail.com>

--- src/pages/SignUp.tsx
+++ src/pages/SignUp.tsx
@@ -5,14 +5,25 @@ import { URL } from '../constants';
 import Input from '../components/auth/Input';
 import useForm from '../hooks/useForm';
 import { postSignUp } from '../api/requests';
+import { useNavigate } from 'react-router-dom';
+import { AxiosError } from 'axios';
 
 export default function SignUp() {
-  const { values, isValid, handleChange } = useForm({ initialValues: { email: '', password: '' } });
+  const navigate = useNavigate();
+  const { values, validValues, handleChange } = useForm({
+    initialValues: { email: '', password: '' },
+  });
+
+  const isButtonEnabled = validValues.email && validValues.password;
 
   const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
     try {
-      await postSignUp(values);
+      const res = await postSignUp(values);
+      if (res.status === 201) {
+        alert('회원가입이 완료되었습니다. 로그인을 진행해주세요.');
+        navigate('/signin');
+      }
     } catch (err) {
       console.error(err);
     }
@@ -27,7 +38,7 @@ export default function SignUp() {
           type='email'
           labelText='이메일'
           value={values.email}
-          isValid={isValid.email}
+          isValid={validValues.email}
           dataTestId={DATA_TEST_ID.INPUT.EMAIL}
           onChange={(e: React.ChangeEvent<HTMLInputElement>) =>
             handleChange({ name: e.target.id, value: e.target.value })
@@ -39,7 +50,7 @@ export default function SignUp() {
           type='password'
           labelText='비밀번호'
           value={values.password}
-          isValid={isValid.password}
+          isValid={validValues.password}
           dataTestId={DATA_TEST_ID.INPUT.PASSWORD}
           onChange={(e: React.ChangeEvent<HTMLInputElement>) =>
             handleChange({ name: e.target.id, value: e.target.value })
@@ -47,9 +58,14 @@ export default function SignUp() {
           errorMsg='8자 이상으로 입력해 주세요'
         />
         <button
-          className='bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline'
+          className={`${
+            isButtonEnabled
+              ? 'bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline'
+              : 'bg-gray-500 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline cursor-not-allowed'
+          }`}
           type='submit'
           data-testid={DATA_TEST_ID.BUTTON.TODO_SUBMIT}
+          disabled={!isButtonEnabled}
         >
           가입하기
         </button>
