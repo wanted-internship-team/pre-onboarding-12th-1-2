8d9fb28ab9216fb28355e79b8508acb3aef36e2e
sjuhan123 <sjuhan123@gmail.com>
2023-08-25 18:05:16 +0900

    REFACTOR: URL 변수 위치 변경 및 변수명 수정
    Co-authored-by: sjuhan123 <sjuhan123@gmail.com>
    Co-authored-by: hyunjinKim <joice.kim7@gmail.com>
    Co-authored-by: mihyunLee <mihy621@gmail.com>
    Co-authored-by: devseop <cocc263@gmail.com>
    Co-authored-by: Dasom Park <devsomda@gmail.com>

--- src/api/axiosInstance.tsx
+++ src/api/axiosInstance.tsx
@@ -1,16 +1,15 @@
 import axios from 'axios';
-
-export const URL = 'https://www.pre-onboarding-selection-task.shop/';
+import { BASE_URL } from '../constants';
 
 // 기본 인스턴스
 export const instance = axios.create({
-  baseURL: URL,
+  baseURL: BASE_URL,
   headers: { 'Content-Type': 'application/json' },
 });
 
 // 토큰이 포함된 접근 인스턴스
 export const accessInstance = axios.create({
-  baseURL: URL,
+  baseURL: BASE_URL,
   headers: {
     'Content-Type': 'application/json',
   },
