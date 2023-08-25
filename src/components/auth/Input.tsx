969490b033e0026633ecbf0fd68520b26073fded
sjuhan123 <sjuhan123@gmail.com>
2023-08-25 18:35:41 +0900

    REFACTOR: 불피요한 JSX 구문 및 주석 삭제
    Co-authored-by: hyunjinKim <joice.kim7@gmail.com>
    Co-authored-by: mihyunLee <mihy621@gmail.com>
    Co-authored-by: devseop <cocc263@gmail.com>
    Co-authored-by: Dasom Park <devsomda@gmail.com>

--- src/components/auth/Input.tsx
+++ src/components/auth/Input.tsx
@@ -24,14 +24,12 @@ export default function Input({
   return (
     <>
       <div className='flex flex-col'>
-        <div className=''>
-          <label
-            htmlFor={id}
-            className='block text-gray-500 font-bold text-left pr-4" for="inline-full-name mb-[4px]'
-          >
-            {labelText}
-          </label>
-        </div>
+        <label
+          htmlFor={id}
+          className='block text-gray-500 font-bold text-left pr-4" for="inline-full-name mb-[4px]'
+        >
+          {labelText}
+        </label>
         <div className='mb-1'>
           <input
             className='bg-gray-200 appearance-none border-2 border-gray-200 rounded w-full py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-purple-500'
@@ -41,7 +39,6 @@ export default function Input({
             placeholder={labelText}
             value={value}
             onChange={onChange}
-            // autoFocus
           />
         </div>
         {!isValid && <p className='text-red-500 text-xs'>{errorMsg}</p>}
--- src/components/todo/TodoItem.tsx
+++ src/components/todo/TodoItem.tsx
@@ -29,73 +29,71 @@ export default function TodoItem({ todoData, onClickUpdate, onClickDelete }: ITo
   };
 
   return (
-    <>
-      <li className='py-2 flex flex-row gap-2.5 justify-start items-center'>
-        <input
-          type='checkbox'
-          checked={isCompleted}
-          onChange={() => {
-            onClickUpdate({ id, todo, isCompleted: !isCompleted });
-          }}
-          className='shrink-0 w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600'
-        />
-        {!isEditMode ? (
-          <div className='min-w-[100px] flex justify-between flex-grow'>
-            <p className='max-w-[520px] border-transparent border-b-2 text-ellipsis overflow-hidden whitespace-nowrap'>
-              {todo}
-            </p>
-            <div className='shrink-0'>
-              <button
-                type='button'
-                data-test-id={DATA_TEST_ID.BUTTON.TODO_MODIFY}
-                onClick={() => setIsEditMode(true)}
-                className='bg-blue-400 hover:bg-blue-500 text-xs text-white font-medium py-1 px-2 mx-1 rounded focus:outline-none focus:shadow-outline'
-              >
-                수정
-              </button>
-              <button
-                type='button'
-                data-test-id={DATA_TEST_ID.BUTTON.TODO_DELETE}
-                onClick={() => onClickDelete({ id })}
-                className='bg-red-400 hover:bg-red-500 text-xs text-white font-medium py-1 px-2 mx-1 rounded focus:outline-none focus:shadow-outline'
-              >
-                삭제
-              </button>
-            </div>
+    <li className='py-2 flex flex-row gap-2.5 justify-start items-center'>
+      <input
+        type='checkbox'
+        checked={isCompleted}
+        onChange={() => {
+          onClickUpdate({ id, todo, isCompleted: !isCompleted });
+        }}
+        className='shrink-0 w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600'
+      />
+      {!isEditMode ? (
+        <div className='min-w-[100px] flex justify-between flex-grow'>
+          <p className='max-w-[520px] border-transparent border-b-2 text-ellipsis overflow-hidden whitespace-nowrap'>
+            {todo}
+          </p>
+          <div className='shrink-0'>
+            <button
+              type='button'
+              data-test-id={DATA_TEST_ID.BUTTON.TODO_MODIFY}
+              onClick={() => setIsEditMode(true)}
+              className='bg-blue-400 hover:bg-blue-500 text-xs text-white font-medium py-1 px-2 mx-1 rounded focus:outline-none focus:shadow-outline'
+            >
+              수정
+            </button>
+            <button
+              type='button'
+              data-test-id={DATA_TEST_ID.BUTTON.TODO_DELETE}
+              onClick={() => onClickDelete({ id })}
+              className='bg-red-400 hover:bg-red-500 text-xs text-white font-medium py-1 px-2 mx-1 rounded focus:outline-none focus:shadow-outline'
+            >
+              삭제
+            </button>
           </div>
-        ) : (
-          <div className='flex justify-between flex-grow'>
-            <input
-              className='border-blue-400 border-b-2 focus:outline-none'
-              type='text'
-              data-test-id={DATA_TEST_ID.INPUT.MODIFY_TODO}
-              onChange={(e: React.ChangeEvent<HTMLInputElement>) => {
-                setEditTodo(e.target.value);
-              }}
-              value={editTodo}
-              autoFocus
-            />
-            <div className='shrink-0'>
-              <button
-                type='button'
-                data-test-id={DATA_TEST_ID.BUTTON.TODO_SUBMIT}
-                onClick={handleClickSubmit}
-                className='bg-blue-400 hover:bg-blue-500 text-xs text-white font-medium py-1 px-2 mx-1 rounded focus:outline-none focus:shadow-outline'
-              >
-                제출
-              </button>
-              <button
-                type='button'
-                data-test-id={DATA_TEST_ID.BUTTON.TODO_CANCEL}
-                onClick={handleClickCancel}
-                className='bg-red-400 hover:bg-red-500 text-xs text-white font-medium py-1 px-2 mx-1 rounded focus:outline-none focus:shadow-outline'
-              >
-                취소
-              </button>
-            </div>
+        </div>
+      ) : (
+        <div className='flex justify-between flex-grow'>
+          <input
+            className='border-blue-400 border-b-2 focus:outline-none'
+            type='text'
+            data-test-id={DATA_TEST_ID.INPUT.MODIFY_TODO}
+            onChange={(e: React.ChangeEvent<HTMLInputElement>) => {
+              setEditTodo(e.target.value);
+            }}
+            value={editTodo}
+            autoFocus
+          />
+          <div className='shrink-0'>
+            <button
+              type='button'
+              data-test-id={DATA_TEST_ID.BUTTON.TODO_SUBMIT}
+              onClick={handleClickSubmit}
+              className='bg-blue-400 hover:bg-blue-500 text-xs text-white font-medium py-1 px-2 mx-1 rounded focus:outline-none focus:shadow-outline'
+            >
+              제출
+            </button>
+            <button
+              type='button'
+              data-test-id={DATA_TEST_ID.BUTTON.TODO_CANCEL}
+              onClick={handleClickCancel}
+              className='bg-red-400 hover:bg-red-500 text-xs text-white font-medium py-1 px-2 mx-1 rounded focus:outline-none focus:shadow-outline'
+            >
+              취소
+            </button>
           </div>
-        )}
-      </li>
-    </>
+        </div>
+      )}
+    </li>
   );
 }
